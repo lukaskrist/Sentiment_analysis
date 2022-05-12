ffa2d42e7be82d060e307becb1128260ea862435
Lukas Kristiansen <73884104+lukaskrist@users.noreply.github.com>
2022-05-12 15:40:34 +0200

    Get_stock_data
    Get data from the stock that is shown to be most profitable

---
+++ Stock_simulate.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 13:25:21 2022
+
+@author: loklu
+"""
+
+
+import pandas as pd
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+
+def stockdata(stock):
+    data = yf.download(stock)
+    ticker = yf.Ticker(stock)
+    data_df = ticker.history(period="7d",interval=("5m"))
+    data_df['Close'].plot(title="TSLA's stock price")
+    return data_df
+
+stockdata('TSLA')
