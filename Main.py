c391b3cb65c46511b29848ae4cb1cbd33b3fb613
Lukas Kristiansen <73884104+lukaskrist@users.noreply.github.com>
2022-06-09 13:01:49 +0200

    Make Dictionary of names and count number

--- Main.py
+++ Main.py
@@ -14,16 +14,20 @@ names = list(names)
 
 d = ec.Dict("en_US")
 for name in names:
-    if d.check(name) == True:
-        names.remove('name')
+    if len(name) != 0:
+        if d.check(name) == True:
+            names.remove(name)
 
 i = 0
-
+dic = {}
 for comment in comments:
     for com in comment:
         for name in names:
-            if str(com).lower() == str(name).lower():
-                i += 1
+            if str(com) == str(name):
+                if name in dic:
+                    dic[name] += 1
+                else:
+                    dic[name] = 1
         
-        
+print(dic)   
 print(names[0:10])
